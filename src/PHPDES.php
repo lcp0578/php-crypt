ec615d299eb0c5639861eaf6e96277e9832db583
李春鹏 <lcp0578@gmail.com>
2016-07-01 00:04:21 +0800

    [ADD] add DES class

---
+++ src/PHPDES.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * DES 
+ *     
+ * @author lichunpeng
+ * @date    2016-06-23 15:16
+ */
+class PHPDES
+{
+    private static function pkcs5_pad ($text, $blocksize) {
+        $pad = $blocksize - (strlen($text) % $blocksize);
+        return $text . str_repeat(chr($pad), $pad);
+    }
+
+    private static function pkcs5_unpad($text) {
+        $pad = ord($text{strlen($text)-1});
+        if ($pad > strlen($text)) {
+            return false;
+        }
+        if (strspn($text, chr($pad), strlen($text) - $pad) != $pad) {
+            return false;
+        }
+        return substr($text, 0, -1 * $pad);
+    }
+
+    public static function encrypt($key, $data) {
+        $size = mcrypt_get_block_size('des', 'ecb');
+        $data = DES::pkcs5_pad($data, $size);
+        $td = mcrypt_module_open('des', '', 'ecb', '');
+        $iv = @mcrypt_create_iv(mcrypt_enc_get_iv_size($td), MCRYPT_RAND);
+        @mcrypt_generic_init($td, $key, $iv);
+        $data = mcrypt_generic($td, $data);
+        mcrypt_generic_deinit($td);
+        mcrypt_module_close($td);
+        return strtoupper(bin2hex($data));
+        //return base64_encode($data);
+        // return $data;
+    }
+
+    public static function decrypt($key, $data) {
+        $data = hex2bin(strtolower($data));
+        
+        $td = mcrypt_module_open('des','','ecb','');
+        $iv = @mcrypt_create_iv(mcrypt_enc_get_iv_size($td), MCRYPT_RAND);
+        $ks = mcrypt_enc_get_key_size($td);
+        @mcrypt_generic_init($td, $key, $iv);
+        $decrypted = mdecrypt_generic($td, $data);
+        mcrypt_generic_deinit($td);
+        mcrypt_module_close($td);
+        $result = DES::pkcs5_unpad($decrypted);
+        return $result;
+    }
+}
